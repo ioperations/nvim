60f4955fc25e24f07ce954190b6211c2265e9552
aodong.qin <ioperations.c@gmail.com>
2023-09-12 22:37:14 +0800

    add ccls navigation

--- lua/servers/clangd.lua
+++ lua/servers/clangd.lua
@@ -31,6 +31,62 @@ if vim.fn.has("mac") == 1 then
     table.insert(fallbackFlags, "-stdlib=libc++")
 end
 
+function nodeRequest(bufnr, method, params, handler)
+    local util = require "lspconfig.util"
+    bufnr = util.validate_bufnr(bufnr)
+    local client = util.get_active_client_by_name(bufnr, "ccls")
+
+    local lspHandler = function(err, result, _, _)
+        if err or not result then
+            vim.notify("No result from ccls", vim.log.levels.WARN, { title = "ccls navigate" })
+            return
+        end
+        handler(result)
+    end
+
+    if client then
+        client.request(method, params, lspHandler)
+    else
+        vim.notify("Ccls is not attached to this buffer", vim.log.levels.WARN, { title = "ccls navigate" })
+    end
+end
+
+local util = require('vim.lsp.util')
+
+function request(method, config)
+    local bufnr = vim.api.nvim_get_current_buf()
+    local params = {
+        textDocument = {
+            uri = vim.uri_from_bufnr(bufnr),
+        },
+        position = {
+            line = vim.fn.getcurpos()[2] - 1,
+            character = vim.fn.getcurpos()[3] - 1,
+        },
+    }
+    params = vim.tbl_extend("keep", params, config)
+
+    --    local json = require "json"
+    --    print("prarm is ", json.encode(params))
+
+
+    local function location_handler(result)
+        if result == nil or vim.tbl_isempty(result) then
+            return nil
+        end
+
+        config = { reuse_win = true, offset_encoding = "utf-32" }
+
+        if vim.tbl_islist(result) then
+            if #result == 1 then
+                -- print("results[1]", json.encode(result))
+                util.jump_to_location(result[1], config.offset_encoding, config.reuse_win)
+            end
+        end
+    end
+    nodeRequest(bufnr, method, params, location_handler)
+end
+
 M.enable = function()
     local capabilities = vim.lsp.protocol.make_client_capabilities()
 
@@ -118,6 +174,27 @@ M.enable = function()
                     F = { "<cmd>CclsMemberFunctionHierarchy float<cr>", "CCls MemberFunctionHierarchy" },
                 },
             }, { prefix = "<leader>" })
+
+            local cmd = vim.api.nvim_create_user_command
+
+            local navigate = function(direction)
+                if type(direction) ~= 'string' then
+                    error("CclsNavigate expect D/R/L/U as params")
+                    if direction ~= 'D' or direction ~= 'R' or direction ~= 'L' or direction ~= 'U' then
+                        error("CclsNavigate expect D/R/L/U as params")
+                    end
+                    return
+                end
+                request("$ccls/navigate", { direction = direction })
+            end
+
+            cmd("CclsNavigate", function(opts)
+                navigate(opts.args)
+            end, { nargs = "*", desc = "ccls Navigation" })
+            vim.keymap.set('n','<c-l>', "<cmd>CclsNavigate D<cr>")
+            vim.keymap.set('n','<c-k>', "<cmd>CclsNavigate L<cr>")
+            vim.keymap.set('n','<c-j>', "<cmd>CclsNavigate R<cr>")
+            vim.keymap.set('n','<c-h>', "<cmd>CclsNavigate U<cr>")
         end,
     }
 
