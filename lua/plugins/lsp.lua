9c8a10fd95a7dfa9bd7afec23a596c8017bbe54b
aodong.qin <ioperations.c@gmail.com>
2023-09-15 20:25:29 +0800

    drop go plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -188,9 +188,6 @@ return {
                         -- "golines",
                         -- "gomodifytags",
                         -- "gotests",
-                        "impl",
-                        "json-to-struct",
-                        "misspell",
                         -- "revive",
                         "prettier",
                         "shellcheck",
@@ -243,27 +240,27 @@ return {
                 })
             end,
         },
-        {
-            "ray-x/go.nvim",
-            dependencies = "ray-x/guihua.lua",
-            lazy = true,
-            config = function()
-                require("go").setup({
-                    lsp_cfg = false,
-                })
-                local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    pattern = "*.go",
-                    callback = function()
-                        require("go.format").goimport()
-                    end,
-                    group = format_sync_grp,
-                })
-            end,
-            event = { "CmdlineEnter" },
-            ft = { "go", "gomod" },
-            build = ':lua require("go.install").update_all()',
-        },
+        -- {
+        --     "ray-x/go.nvim",
+        --     dependencies = "ray-x/guihua.lua",
+        --     lazy = true,
+        --     config = function()
+        --         require("go").setup({
+        --             lsp_cfg = false,
+        --         })
+        --         local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+        --         vim.api.nvim_create_autocmd("BufWritePre", {
+        --             pattern = "*.go",
+        --             callback = function()
+        --                 require("go.format").goimport()
+        --             end,
+        --             group = format_sync_grp,
+        --         })
+        --     end,
+        --     event = { "CmdlineEnter" },
+        --     ft = { "go", "gomod" },
+        --     build = ':lua require("go.install").update_all()',
+        -- },
         -- lsp server manager: automatic setup
         {
             "williamboman/mason-lspconfig.nvim",
