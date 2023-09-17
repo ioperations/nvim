2b0a22f8b7ea1a14dec57e5244e548fad41832b4
aodong.qin <ioperations.c@gmail.com>
2023-09-17 12:40:42 +0800

    add vimspector

---
+++ lua/plugins/vimspector.lua
@@ -0,0 +1,8 @@
+return {
+    "puremourning/vimspector",
+    config = function()
+        _G.vimspector_enable_mappings = "HUMAN"
+        _G.vimspector_install_gadgets = { "debugpy", "vscode-cpptools", "CodeLLDB" }
+        _G.vimspector_base_dir = vim.fn.expand("$HOME/.locol/share/nvim/vimspector-config")
+    end,
+}
