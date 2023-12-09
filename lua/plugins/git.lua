87024ee6b7e7ef441d9d8f08e97b8e120ab96ed3
aodong.qin <ioperations.c@gmail.com>
2023-12-09 11:26:40 +0800

    enable current line blame by default

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -38,14 +38,14 @@ return {
                     follow_files = true,
                 },
                 attach_to_untracked = true,
-                current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+                current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
                 current_line_blame_opts = {
                     virt_text = true,
                     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
                     delay = 10,
                     ignore_whitespace = false,
                 },
-                current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+                current_line_blame_formatter = "            <author_mail>, <author_time:%Y-%m-%d> - <summary>",
                 sign_priority = 6,
                 update_debounce = 100,
                 status_formatter = nil, -- Use default
