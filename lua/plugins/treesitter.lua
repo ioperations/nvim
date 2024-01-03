f2639a2242e6d16ebf0ac01790dba1510dca3e94
aodong.qin <ioperations.c@gmail.com>
2024-01-03 21:53:44 +0800

    iPhish/rainbow-delimiters.nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,37 @@ return {
     { "mmarchini/bpftrace.vim" },
     { "whatyouhide/vim-tmux-syntax" },
     {
+        "HiPhish/rainbow-delimiters.nvim",
+        config = function()
+            -- This module contains a number of default definitions
+            local rainbow_delimiters = require("rainbow-delimiters")
+            require("rainbow-delimiters.setup").setup({
+                strategy = {
+                    -- ...
+                    [""] = rainbow_delimiters.strategy["global"],
+                    vim = rainbow_delimiters.strategy["local"],
+                },
+                query = {
+                    [""] = "rainbow-delimiters",
+                    lua = "rainbow-blocks",
+                },
+                highlight = {
+                    -- ...
+                    "RainbowDelimiterRed",
+                    "RainbowDelimiterYellow",
+                    "RainbowDelimiterBlue",
+                    "RainbowDelimiterOrange",
+                    "RainbowDelimiterGreen",
+                    "RainbowDelimiterViolet",
+                    "RainbowDelimiterCyan",
+                },
+            })
+        end,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+        },
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         event = { "BufReadPost", "BufNewFile" },
