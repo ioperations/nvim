89e97c0d38532773b25b730f0cadf125ce0e0089
aodong.qin <ioperations.c@gmail.com>
2024-01-26 09:50:19 +0800

    build

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -270,6 +270,7 @@ return {
                     config = function()
                         vim.g.go_doc_keywordprg_enabled = 0
                     end,
+                    build = ":GoInstallBinaries",
                 },
             },
             lazy = true,
