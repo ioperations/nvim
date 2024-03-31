3dcc762cb49daacaf32871ad8792ff34896aefa0
aodong.qin <ioperations.c@gmail.com>
2024-03-31 14:02:07 +0800

    [coverage] add nvim-coverage plugin

---
+++ lua/plugins/coverage.lua
@@ -0,0 +1,26 @@
+return {
+    "andythigpen/nvim-coverage",
+    dependencies = "nvim-lua/plenary.nvim",
+    config = function()
+        require("coverage").setup({
+            commands = true, -- create commands
+            highlights = {
+                -- customize highlight groups created by the plugin
+                covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
+                uncovered = { fg = "#F07178" },
+            },
+            signs = {
+                -- use your own highlight groups or text markers
+                covered = { hl = "CoverageCovered", text = "▎" },
+                uncovered = { hl = "CoverageUncovered", text = "▎" },
+            },
+            summary = {
+                -- customize the summary pop-up
+                min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
+            },
+            lang = {
+                -- customize language specific settings
+            },
+        })
+    end,
+}
