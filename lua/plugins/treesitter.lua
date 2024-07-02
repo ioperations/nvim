035f66e86c856e7e76ac247bfd3acd63dfc5511c
aodong.qin <ioperations.c@gmail.com>
2024-07-02 22:44:45 +0800

    [nvim-ts-rainbow] disable

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -63,7 +63,7 @@ return {
             "nvim-treesitter/nvim-treesitter-textobjects",
             -- "windwp/nvim-ts-autotag", -- auto close and rename tags
             "simonward87/nvim-ts-autotag",
-            "christianchiarulli/nvim-ts-rainbow",
+            --"christianchiarulli/nvim-ts-rainbow",
         },
 
         opts = {
