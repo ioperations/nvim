11faf1d43a1c0617081db048046183ee3b2563ca
aodong.qin <ioperations.c@gmail.com>
2024-09-02 21:30:37 +0800

    [persistence.nvim] add

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -197,4 +197,13 @@ return {
             cmd = "AsyncRun",
         },
     },
+    -- persistent
+    -- Lua
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre", -- this will only start session saving when an actual file was opened
+        opts = {
+            -- add any custom options here
+        },
+    },
 }
