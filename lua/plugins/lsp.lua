93c695084169884b0339a05477f77ea3fc41b6fc
aodong.qin <ioperations.c@gmail.com>
2025-12-25 07:48:20 +0800

    [lsp_lines.nvim] enable

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -53,9 +53,13 @@ return {
 
         {
             "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-            enabled = false,
+            enabled = true,
             config = function()
                 require("lsp_lines").setup()
+                -- Disable virtual_text since it's redundant due to lsp_lines.
+                vim.diagnostic.config({
+                    virtual_text = false,
+                })
             end,
         },
         {
