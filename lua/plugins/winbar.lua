9761df5e0ffb317d05d43c98648a661a18cced8b
aodong.qin <ioperations.c@gmail.com>
2026-02-12 08:33:16 +0800

    barbecure reduce cursorMove event

--- lua/plugins/winbar.lua
+++ lua/plugins/winbar.lua
@@ -82,6 +82,29 @@ return {
         end,
     },
     config = function()
-        require("barbecue").setup()
+        -- triggers CursorHold event faster
+
+        -- vim.opt.updatetime = 200
+
+        require("barbecue").setup({
+            create_autocmd = false, -- prevent barbecue from updating itself automatically
+        })
+
+        vim.api.nvim_create_autocmd({
+            "WinScrolled", -- or WinResized on NVIM-v0.9 and higher
+            "BufWinEnter",
+            "CursorHold",
+            "InsertLeave",
+
+            -- include this if you have set `show_modified` to `true`
+            "BufModifiedSet",
+        }, {
+            group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+            callback = function()
+                require("barbecue.ui").update()
+            end,
+        })
+
+        -- require("barbecue").setup()
     end,
 }
